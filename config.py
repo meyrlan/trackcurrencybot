f9b819cd5909a3e55946aec6bb77740eb99796dd
Meiirlan Suiirkhanov <45965276+meyrlan@users.noreply.github.com>
2021-10-14 15:03:57 +0600

    Delete config.py

--- config.py
+++
@@ -1,4 +0,0 @@
-binance_api_key = 'cOn0PAN2xT0y1o9GyZhxEeC70w0MvG9dh04u3pMjL4GL1ihcvhf9XUspSYicb2EP'
-binance_api_security = 'kR74W0ZaX6uOE9NjgzYrEYUsTXY1qeRPyPjOu9vixOUdJNir5EJZf2Oymh0ek1YS'
-bot_api_key = '2077884272:AAFS6zm80M87B6MEJcQFfWt-u7oSA-5s9O4'
-currency = 'USDT'
